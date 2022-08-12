1c2f156503fcadc3c0060ef98fdd1f40620e29c2
TEISTU <TEISTU@DESKTOP-Q3H3D8M>
2022-08-12 09:48:20 +0200

    checkpoint: added @GetMapping to CustomerController -> listCustomers
    method

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.ICustomerDAO;
@@ -21,7 +22,7 @@ public class CustomerController {
 	
 	
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 			
 		// get customers from the DAO
